b6508b1ec0ca6c3d6578c04ab0d86a1c7850359f
SonjiaD <147272781+SonjiaD@users.noreply.github.com>
2025-06-16 23:49:41 -0700

    Merge remote-tracking branch 'origin/outfit-shop'
    - there are no merge conflicts on the outfit page
    -now need to merge 1 more branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,39 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ReadingPage from './pages/ReadingPage';
 import FocusTracker from './pages/FocusTracker';
-// import OutfitShop from './pages/OutfitShop';
-import './App.css';
+import OutfitShop from './pages/OutfitShop';
 
-const App: React.FC = () => {
+
+function App() {
+  // const [msg, setMsg] = useState('');
+  // const [response, setResponse] = useState('');
+
+  // const handleSend = async () => {
+  //   const res = await fetch("http://localhost:5000/api/echo", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ message: msg })
+  //   });
+  //   const data = await res.json();
+  //   setResponse(data.response);
+  // };
+
   return (
     <Router>
       <nav style={{ marginBottom: '1rem' }}>
-        <Link to="/">Home</Link>
-        <Link to="/reading">Reading App</Link>
-        <Link to="/focus">Focus Tracker</Link>
-        <Link to="/outfit-shop">Outfit Shop</Link>
+        <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+        <Link to="/pdf">PDF Upload</Link>
+        <Link to ="/focus" style={{ marginLeft: '1rem' }}>Focus Tracker</Link>
+        <Link to="/outfit-shop" style={{ marginLeft: '1rem' }}>Outfit Shop</Link>
+        <Link to="/reading" style={{ marginLeft: '1rem' }}>Reading</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<div>Welcome to the Home Page!</div>} />
         <Route path="/reading" element={<ReadingPage />} />
         <Route path="/focus" element={<FocusTracker />} />
-        {/* <Route path="/outfit-shop" element={<OutfitShop />} /> */}
+        <Route path="/outfit-shop" element={<OutfitShop />} />
+        <Route path="/reading" element={<ReadingPage />} />
       </Routes>
     </Router>
   );
