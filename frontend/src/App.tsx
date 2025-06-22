f2ee1a81bdf81f5555dd4685036458d6234b1d44
Sara Gebara <saragebaraa@gmail.com>
2025-06-22 03:25:10 -0400

    Added avatar customization page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,7 @@ import Signup from './pages/Signup';
 import TeacherHome from './pages/TeacherHome';
 import MyClass from './pages/MyClass';
 import StudentOverview from './pages/StudentOverview';
+import CustomizeAvatar from './pages/CustomizeAvatar';
 
 function App() {
   // const [msg, setMsg] = useState('');
@@ -29,21 +30,8 @@ function App() {
 
   return (
     <Router>
-      <nav style={{ marginBottom: '1rem' }}>
-        <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
-        <Link to="/pdf">PDF Upload</Link>
-        <Link to ="/focus" style={{ marginLeft: '1rem' }}>Focus Tracker</Link>
-        <Link to="/questionnaire" style={{ marginLeft: '1rem' }}>Questionnaire</Link>
-        <Link to="/welcome" style={{ marginLeft: '1rem' }}>Welcome</Link>
-        <Link to="/home" style={{ marginLeft: '1rem' }}>HomePage</Link>
-        <Link to="/loginsignup" style={{ marginLeft: '1rem' }}>Login/Signup</Link>
-        <Link to="/outfit-shop" style={{ marginLeft: '1rem' }}>Outfit Shop</Link>
-        <Link to="/reading" style={{ marginLeft: '1rem' }}>Reading</Link>
-        <Link to="/teacherhome" style={{ marginLeft: '1rem' }}>Teacher Home</Link>
-      </nav>
-
       <Routes>
-        <Route path="/" element={<div>Welcome to the Home Page!</div>} />
+        <Route path="/" element={<LoginSignup/>} />
         <Route path="/reading" element={<ReadingPage />} />
         <Route path="/focus" element={<FocusTracker />} />
         <Route path="/questionnaire" element={<Questionnaire />} />
@@ -57,6 +45,7 @@ function App() {
         <Route path="/teacherhome" element={<TeacherHome />} />
         <Route path="/myclass" element={<MyClass />} />
         <Route path="/studentoverview" element={<StudentOverview />} />
+        <Route path="/avatar" element={<CustomizeAvatar />} />
         
       </Routes>
     </Router>
---
+++ frontend/src/pages/CustomizeAvatar.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import "./CustomizeAvatar.css";
+import logo from '../assets/logo.png'; 
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import bearPfp from '../assets/bear-avatar.png';
+import coinCount from '../assets/coin-count.png';
+import chef from '../assets/chef.png';
+import pirate from '../assets/pirate.png';
+import hero from '../assets/hero.png';
+
+const Home: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="customavatar-container">
+      <header className="navbar">
+        <div className="logo">
+          <img src={logo} alt="Logo" className="logo-img" />
+        </div>
+        <nav>
+          <Link to={'/reading'} className="nav-link">{'Read'}</Link>
+          <Link to={'/home'} className="nav-link">{'Dashboard'}</Link>
+          <Link to={'/outfit-shop'} className="nav-link">{'Outfit Shop'}</Link>
+          <button className="logout-nav-btn" onClick={() => navigate('/LoginSignup')}>Log Out</button> 
+        </nav>
+      </header>
+
+      <main className="customavatar-main">
+        <h1>Customize your avatar!</h1>
+        <div className="coins-display">
+            <img src={coinCount} alt="Coins" className="coin-img" />
+        </div>
+        
+        <div className="avatar-sections">
+          <div className="avatar-equipment">
+            <img src={bearPfp} alt="Bear Avatar" className="bear-pfp" />
+            <h2>Lily</h2>
+            <p>equipped <em>Honey Bear</em></p>
+          </div>
+
+          <div className="outfit-gallery">
+            <h2>Outfits</h2>
+            <div className="outfit-row">
+              <img src={chef} alt="Chef Outfit" className="outfit-img" />
+              <img src={pirate} alt="Pirate Outfit" className="outfit-img" />
+              <img src={hero} alt="Superhero Outfit" className="outfit-img" />
+            </div>
+            <button className="save-changes">Save Changes</button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
